4e9ff5d1b4e0e4dbbc55397e97cc578cea352dc4
John Truong <viajohnt@gmail.com>
2023-05-29 19:36:58 -0600

    Final Commit

--- server/app.py
+++ server/app.py
@@ -22,13 +22,21 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
-
+    articles = [articles.to_dict() for article in articles]
+    return make_response(
+        jsonify(articles),
+        200
+    )
+    
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] += 1
 
-    pass
+    if session['page_views'] <= 3:
+        return Article.query.filter(Article.id == id).first().to_dict(), 200
+        
+    return {'message': 'Maximum pageview limit reached'}, 401
 
 if __name__ == '__main__':
     app.run(port=5555)
